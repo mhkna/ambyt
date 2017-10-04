c195e102dbae09c24c0f297ab01e0e173ecea318
Michael <michkna3@gmail.com>
2017-10-04 15:50:51 -0500

    geocoder bounding box added temporarily until can figure out a better one for anon users. also a coords method was added to the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,13 @@ class Post < ApplicationRecord
   validates :content, presence: true, length: { maximum: 210 }
   after_validation :geocode#, :if => :address_changed
 
+  def coords
+    [self.latitude, self.longitude]
+  end
+
+  def nearest_users(distance)
+    view_area = Geocoder::Calculations.bounding_box(self.coords, distance)
+    #User.within_bounding_box(view_area)
+  end
+
 end
