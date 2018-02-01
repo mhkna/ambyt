4af2a258216063b47f347ba80caaaac4537b4e90
Michael <michkna3@gmail.com>
2018-02-01 16:29:11 -0600

    add association btw post and user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   acts_as_votable
+  belongs_to :user
   has_many :comments, dependent: :destroy
   validates :content, presence: true,
                      length: { minimum: 2 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201221712) do
+ActiveRecord::Schema.define(version: 20180201222800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20180201221712) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
