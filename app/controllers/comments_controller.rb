11635178a59f02c9f95817c640aef6c2486fcadd
Michael <michkna3@gmail.com>
2018-02-12 12:25:34 -0600

    removed temp coords from posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,7 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(comment_params)
-    if @comment.save
-      @post.touch
-    end
+    @comment = @post.comments.create(comment_params)
 
     redirect_to post_path(@post)
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :post#, touch: true
+  belongs_to :post, touch: true
 
   validates :content, presence: true, length: { maximum: 210 }
 end
