673be0e436680c2fcdc53b9c14cb7ab74c920ed8
Michael <michkna3@gmail.com>
2018-02-12 20:15:20 -0600

    ability cancancan revert for heroku

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,5 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    if user.admin_role?
-      can :manage, :all
-    end
-    if user.user_role?
-    end
   end
 end
