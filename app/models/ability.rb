e2671df28174eee3f77d8b566ca252c4ccea89cf
Michael <michkna3@gmail.com>
2018-02-13 16:52:35 -0600

    voting authorization fixed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,8 @@ class Ability
   def initialize(user)
     can :read, :all
     if user
-      can [:create, :destroy, :update, :upvote, :downvote], [Post, Comment], user_id: user.id
+      can [:upvote, :downvote], Post
+      can [:create, :destroy, :update], [Post, Comment], user_id: user.id
     end
     if user && user.admin
       can :manage, :all
