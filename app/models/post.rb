8024977ed22c4954e280486da72b0e6ba0f2ca42
Michael <michkna3@gmail.com>
2018-02-22 11:38:22 -0600

    file size validation for image uploads

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   has_attached_file :picture, styles: { normal: "150x200>" }, default_url: "/images/:style/none.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+  validates_with AttachmentSizeValidator, attributes: :original, less_than: 1.megabytes
   paginates_per 7
   acts_as_votable
   belongs_to :user
