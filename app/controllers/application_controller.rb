691eca2652719eb377af57c1513a3f1b18b61228
Michael <michkna3@gmail.com>
2018-02-23 15:22:41 -0600

    removed request.remote_ip from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,6 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def remote_ip
-    if request.remote_ip == '127.0.0.1'
-      '78.125.0.209'
-    else
-      request.remote_ip
-    end
-  end
-
   protected
 
   def configure_permitted_parameters
