9b9703475a4dbf12a98bb468c3634ef1b10df66b
Michael <michkna3@gmail.com>
2018-02-23 18:08:42 -0600

    anon check moved to app_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def name_or_anon(obj)
+    obj.user != nil && obj.user.name != nil && obj.user.name != "" ? obj.user.name : "anonymous"
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,2 @@
 module PostsHelper
-  def name_or_anon(obj)
-    obj.user != nil && obj.user.name != nil && obj.user.name != "" ? obj.user.name : "anonymous"
-  end
 end
